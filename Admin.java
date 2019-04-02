ad5668f29b4e041e6e2b00cce0bcefc45632edc4
zhangchangshengzcs <422540002@qq.com>
2019-04-02 11:33:18 +0800

    update Admin.java file

--- Admin.java
+++ Admin.java
@@ -1,6 +1,8 @@
 import java.io.Serializable;
+import java.util.Date;
 
 public class Admin implements Serializable{
     private string adminid;
     private String password;
+    private Date lastLogin;
 }
