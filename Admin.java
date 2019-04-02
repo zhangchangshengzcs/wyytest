7c944718e0d1d74719c4ae1093db710c51fc03af
zhangchangshengzcs <422540002@qq.com>
2019-04-02 11:36:42 +0800

    1号提交admin.java file

--- Admin.java
+++ Admin.java
@@ -3,4 +3,6 @@ import java.io.Serializable;
 public class Admin implements Serializable{
     private string adminid;
     private String password;
+    private Integer flag;
+    private String name;
 }
